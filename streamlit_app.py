deac612997127bb47cd1c04d33d8f4ce51cf07d2
Jon Exume <jonexume@me.com>
2022-10-28 15:37:31 -0400

    added s panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # created the main python file
 
 import streamlit
+import pandas
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -15,3 +16,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
