1c99a12db079ef8cf8b45ddf56a50b333163251d
Jon Exume <jonexume@me.com>
2022-10-28 10:55:31 -0400

    added more info to streamlit
    additional things we added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 streamlit.text('This stuff is amazing')
 streamlit.text('I love what is happening in this chapter')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
