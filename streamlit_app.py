8f33934efda06bc32bac62668a0f0d8dad37665b
Jon Exume <jonexume@me.com>
2022-10-28 15:34:08 -0400

    added one more item
    One more item added

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmaeal')
 streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
