150ad8ac2f1634b717b4fc48eb4112bbf0d3f0fb
Jon Exume <jonexume@me.com>
2022-11-06 15:53:34 -0500

    adding snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
