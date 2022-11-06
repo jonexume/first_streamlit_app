c48e22283b37d0902ffc57fe9600f0ce03a4df4a
Jon Exume <jonexume@me.com>
2022-11-05 22:29:35 -0400

    Added some new content
    New items for this snowflake tutorial I am going through

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,15 @@
 
 import streamlit
 import pandas
+import requests
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmaeal')
